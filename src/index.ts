6f09c7e6cb405f8967b69262f80de3b9e8c01420
unknown <glhrmpaiva@gmail.com>
2020-10-21 17:14:43 -0300

    Deploy

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 
 app.use('/user', userRouter)
 app.use('/image', imageRouter)
