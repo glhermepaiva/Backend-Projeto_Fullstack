24e92e65fee6d7e85ecbc5982a277e95d9945580
unknown <glhrmpaiva@gmail.com>
2020-10-21 17:11:29 -0300

    Deploy

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,13 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(
-  cors({
-      credentials: true,
-      origin: [
-          'http://localhost:3000',
-          'https://flickenu.herokuapp.com'
-      ]
-  }),
-)
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use('/user', userRouter)
 app.use('/image', imageRouter)
